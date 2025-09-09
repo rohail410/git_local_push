f471facf7236f85be8427dc8824b93f47b2afcbc
Rohail Tabassum <rohailtabassumit@gmail.com>
2025-09-09 19:28:43 +0500

    Adding a conflicting line in main branch

--- intro.py
+++ intro.py
@@ -1,3 +1,4 @@
 print("My name is Something Something")
 print("Making changes only on all branches")
 print("I am adding these change from feature_2 without conflicts")
+print("This will create conflict main branch")
