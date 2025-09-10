7bed18f70ddbce6ff89109a0b9c7fb9498333e31
Rohail Tabassum <rohailtabassumit@gmail.com>
2025-09-10 14:39:51 +0500

    Conflict pls

--- intro.py
+++ intro.py
@@ -1,4 +1,4 @@
-print("My name is Something Something")
+print("This is something else")
 print("Making changes only on all branches")
 print("This will create conflict with main branch. rohail-branch")
 print("I am adding these change from feature_2 without conflicts")
