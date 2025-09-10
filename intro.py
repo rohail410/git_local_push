27879c47f3a1ebff768486f28a5bdd5ead95b8f2
Rohail Tabassum <rohailtabassumit@gmail.com>
2025-09-10 14:48:14 +0500

    Another conflict stategy for me

--- intro.py
+++ intro.py
@@ -3,3 +3,5 @@ print("Making changes only on all branches")
 print("This will create conflict with main branch. rohail-branch")
 print("I am adding these change from feature_2 without conflicts")
 # This should definately create a conflict
+
+# Another conflict stategy. 2
